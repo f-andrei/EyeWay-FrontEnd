3a48b5e062798b0056aea89a9f306efc5ea049b4
GUTOMADON <gutomadon@gmail.com>
2024-09-05 19:33:05 -0300

    update  statistics.js

--- EyeWay/src/pages/Statistics.js
+++ EyeWay/src/pages/Statistics.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { PieChart } from 'react-native-chart-kit'; // Importa o PieChart, forma o grafico de pizza
 import Navbar from '../components/Navbar';
+import { Dimensions } from 'react-native'; 
+//tive que instalar no cmd:
+//1 - react-native-chart-kit: Fornece vários tipos de gráficos, incluindo gráficos de pizza.
+//2 - react-native-svg: É uma dependência necessária para renderizar gráficos baseados em SVG.
 
 export default function Statistics({ navigation }) {
-  // adicionar dados para o gráfico
+  // Dados para o gráfico de pizza
   const data = [
-    // arrumar depois
+    //O gráfico recebe um array data porque essa é a maneira pela qual os dados a serem representados no gráfico são passados. 
+    //Cada elemento do array representa uma fatia do gráfico de pizza, e o conteúdo do array determina as informações que serão exibidas.
+    { name: 'Infração A', population: 45, color: '#FF6384', legendFontColor: '#FFFFFF', legendFontSize: 15 },
+    { name: 'Infração B', population: 25, color: '#36A2EB', legendFontColor: '#FFFFFF', legendFontSize: 15 },
+    { name: 'Infração C', population: 30, color: '#FFCE56', legendFontColor: '#FFFFFF', legendFontSize: 15 },
   ];
+
+  // Largura da tela para ajustar o tamanho do gráfico
+  const screenWidth = Dimensions.get('window').width;
 
   return (
     <View style={styles.container}>
@@ -24,8 +36,19 @@ export default function Statistics({ navigation }) {
         </View>
 
         <View style={styles.containerGrafico}>
-          {/* adicionar o gráfico */}
-          <Text style={styles.textoInformativo}>Gráfico será exibido aqui.</Text>
+          {/* Gráfico de Pizza */}
+          <PieChart
+            data={data}
+            width={screenWidth - 40} // Defina a largura como a largura da tela menos margem
+            height={220} // Altura do gráfico
+            chartConfig={{
+              color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            }}
+            accessor={"population"} // Campo que será utilizado para o tamanho da fatia
+            backgroundColor={"transparent"}
+            paddingLeft={"15"}
+            absolute // Mostra os valores absolutos no gráfico
+          />
         </View>
       </ScrollView>
 
