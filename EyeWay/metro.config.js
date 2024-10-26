41486bf292fc973192c615730928eff6bf4a6dcd
Andrei <andreifrancap@gmail.com>
2024-10-26 14:19:10 -0300

    config

--- EyeWay/metro.config.js
+++ EyeWay/metro.config.js
@@ -2,6 +2,14 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = {
+  ...defaultConfig,
+  resolver: {
+    ...defaultConfig.resolver,
+    assetExts: [...defaultConfig.resolver.assetExts, 'png', 'jpg', 'jpeg', 'gif']
+  }
+};
 
 module.exports = config;
