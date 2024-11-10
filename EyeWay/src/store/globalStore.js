55924ca1c4e2bc4a6546e33050cab50fb593392d
Luis Fernando dos Santos <luisf3rnando29@outlook.com>
2024-11-10 18:36:35 -0300

    att: userId on store

--- EyeWay/src/store/globalStore.js
+++ EyeWay/src/store/globalStore.js
@@ -4,8 +4,9 @@ import { create } from "zustand";
 export const useStore = create((set) => ({
     isAuthenticated: false,
 
-    setAuthenticated: (authenticated) => set((state) => ({ isAuthenticated: authenticated }))
-
+    setAuthenticated: (authenticated) => set((state) => ({ isAuthenticated: authenticated })),
+    setUserId: (user_id) => set((state) => ({ user_id: user_id})),
+    user_id: 0,
   }))
   
   
