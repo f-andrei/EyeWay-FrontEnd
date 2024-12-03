6cedfa9737db294353049e0202fc57e7bdea0fb0
Andrei <andreifrancap@gmail.com>
2024-12-03 15:45:16 -0300

    Removido listagem de infrações manuais (foi integrado a página alertas)

--- EyeWay/src/components/Navbar.js
+++ EyeWay/src/components/Navbar.js
@@ -61,7 +61,7 @@ export default function Navbar({ navigation }) {
       </TouchableOpacity>
 
       <TouchableOpacity
-        onPress={() => navigation.navigate('Infractions')}
+        onPress={() => navigation.replace('Infractions')}
         style={[
           estilos.navItem,
           isWeb && estilos.webNavItem,
@@ -95,24 +95,6 @@ export default function Navbar({ navigation }) {
           isWeb && estilos.webNavTexto
         ]}>Operações</Text>
       </TouchableOpacity>
-
-      <TouchableOpacity
-        onPress={() => navigation.navigate('ManualInfractions')}
-        style={[
-          estilos.navItem,
-          isWeb && estilos.webNavItem,
-        ]}
-      >
-        <View style={estilos.webIconContainer}>
-        <AntDesign name="enviroment" size={isWeb ? 21:30} color="white" />
-        </View>
-        <Text style={[
-          estilos.navTexto,
-          isWeb && estilos.webNavTexto
-        ]}>Manual Infractions</Text>
-      </TouchableOpacity>
-
-
       <TouchableOpacity
         onPress={() => navigation.navigate('Statistics')}
         style={[
